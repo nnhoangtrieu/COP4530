1ba76f11ad9ca15d706afd3c4e72013bca9ed32e
Trieu.Nguyen <80027464@hpctpa3pl0001.cm.cluster>
2024-01-23 22:27:27 -0500

    update lec1

--- lec1.cpp
+++ lec1.cpp
@@ -4,48 +4,44 @@
 using namespace std;
 
 
+void learn_pointers() { 
+    char ch = 'Q'; 
 
-// void learn_pointers() { 
-//     char ch = 'Q'; 
+    char *p = &ch; 
 
-//     char *p = &ch; 
+    cout << *p; // outputs 'Q'
 
-//     cout << *p; // outputs 'Q'
+    ch = 'Z';
 
-//     ch = 'Z';
+    cout << *p; // outputs 'Z' 
 
-//     cout << *p; // outputs 'Z' 
+    *p = 'X';
 
-//     *p = 'X';
-
-//     cout << ch; // outputs 'X'
+    cout << ch; // outputs 'X'
     
-// }
+}
 
-// void learn_array() 
-// {
-//     double f[5]; 
-//     int m[10];
 
-//     f[4] = 2.5;
-//     m[2] = 4;
+void learn_array() 
+{
+    double f[5]; 
+    int m[10];
 
-//     cout << f[m[2]];
+    f[4] = 2.5;
+    m[2] = 4;
 
-//     int a[] = {1, 2, 3, 4};
-//     bool b[] = {false, true};
-//     char c[] = {'c', 'a', 't'};
+    cout << f[m[2]];
 
-//     char *p = c; // p points to c[0] 
-//     char *q = &c[0]; // q also points to c[0] 
+    int a[] = {1, 2, 3, 4};
+    bool b[] = {false, true};
+    char c[] = {'c', 'a', 't'};
 
-//     cout << c[2] << p[2] << q[2]; // print 'ttt'
+    char *p = c; // p points to c[0] 
+    char *q = &c[0]; // q also points to c[0] 
+
+    cout << c[2] << p[2] << q[2]; // print 'ttt'
     
-// }
-
-
-
-
+}
 
 
 void learn_structure() 
@@ -64,8 +60,49 @@ void learn_structure()
     pass1.name = "Trieu Ngoc Hoang Nguyen"; // change name
 
     cout << pass1.name;
+
+
+    // Structure new Operator 
+
+    Passenger *p; 
+    p = new Passenger; // p points to the new Passenger
+
+    p->name = 'Vy'; 
+    p->mealPref = REGULAR; 
+
+    delete p; // destroy the object p points to 
+
 }
 
+
+void learn_buffer() 
+{
+    char *buffer = new char[500]; // allocate a buffer of 500 chars
+    buffer[3] = 'a';              // still accessed using []
+    delete [] buffer; 
+
+    /*
+    - If an object is allocated with new, it should eventually be 
+    deallocated with 'delete'
+    - Inaccessible objects in dynamic memory can cause a memory leak
+    */
+}
+
+
+/*
+- By default, arguments in C++ programs are passed by value.
+=> The system only makes a copy of the variable to be passed to the function
+=> Modification to this copy does not alter the actual argument 
+
+===> To modify an argument, a pointer must be passed
+*/
+
+
+void learn_argument_passing(int value, int &ref) 
+{
+    value++;        // Has no effect on the actual argument
+    ref++;          // Modifies the actual argument 
+}
 int main() 
 { 
 
