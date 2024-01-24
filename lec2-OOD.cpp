ad77170bc3d7716bfaa97217b051886ed62f7866
Trieu.Nguyen <80027464@hpctpa3pl0001.cm.cluster>
2024-01-24 10:02:09 -0500

    finish lec2

--- lec2-OOD.cpp
+++ lec2-OOD.cpp
@@ -25,6 +25,10 @@ from another class
 
 */
 
+
+/*
+
+*/
 #include <string> 
 #include <iostream> 
 
@@ -38,19 +42,22 @@ class Person {
     string idNum;
 
     public: 
-    Person(const string& newName, const string &newIdNum);
+    Person(const string& name, const string &idNum);
     void print();
     string get_name();
 };
 
 
-Person::Person(const string& newName, const string& newIdNum) : name(newName), idNum(newIdNum) {}
+Person::Person(const string& name, const string& idNum) : name(name), idNum(idNum) {}
 
 
 void Person::print() {
     cout << "Name: " << name << endl;
+    cout << "Id Number: " << idNum << endl;
 }
 
+
+string Person::get_name() {return name;};
 
 // Subclass 
 class Student : public Person {
@@ -59,15 +66,15 @@ class Student : public Person {
     int gradYear; 
 
     public: 
-    Student(const string& newName, const string& newIdNum, const string& newMajor, const int gradYear);
+    Student(const string& name, const string& idNum, const string& major, const int gradYear);
     void print();
     void changeMajor(const string& newMajor);
 };
 
 
 Student::Student(
-    const string& newName, const string& newIdNum, const string& newMajor, const int gradYear
-) : Person(newName, newIdNum), major(newMajor), gradYear(gradYear) {}
+    const string& name, const string& idNum, const string& major, const int gradYear
+) : Person(name, idNum), major(major), gradYear(gradYear) {}
 
 
 
@@ -79,17 +86,129 @@ void Student::print()
 }
 
 
+void Student::changeMajor(const string& newMajor) {
+    cout << "Change major from " << major << "to " << newMajor << endl;
+    cout << "/////////////////////////////////" << endl;
+    major = newMajor;
+    cout << "New information: " << endl;
+    Student::print();
+}
+
+
+///////////////////// Template ////////////////////////////
+template <typename T> 
+
+T genericMin(T a, T b) {
+    return (a < b ? a : b);
+}
+
+
+// Class template for a generic Pair 
+template <typename T1, typename T2> 
+
+class Pair {
+    public: 
+        Pair(T1 first, T2 second) : firstValue(first), secondValue(second) {}
+
+        void display() const {
+            cout << "Pair: " << firstValue << ", " << secondValue << endl;
+        }
+
+    private: 
+        T1 firstValue; 
+        T2 secondValue;
+};
+
+
+///////////////////////// Exceptions ////////////////////////////////
+
+class MathException {
+    public: 
+        MathException(const string& err) : errMsg(err) {} 
+
+        string  getError() {return errMsg;}
+
+    private: 
+        string errMsg;
+}
+
+class ZeroDivision : public MathException {
+    public: 
+        ZeroDivision(const string &err) : MathException(err) {}
+};
+
+class NegativeRoot : public MathException {
+    public: 
+        NegativeRoot(const string& err) : MathException(err) {}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main()
 {
-    Person person("Trieu", "12");
-    person.print();
+    // Person person("Trieu", "12");
+    // person.print();
 
-    Student student1("Trieu", "12", "Computer Science", 2026);
-    student1.print();
+    // Student student1("Trieu", "12", "Computer Science", 2026);
+    // student1.print();
+
+    // student1.changeMajor("Math");
 
 
     //////////////////////////////////////////////////////////////
 
 
-    
+    // Person *pp[100];        // array of 100 Person pointers
+
+    // pp[0] = new Person("Trieu", "12");
+    // pp[1] = new Student("Vy", "13", "Marketing", 2025); // add a Student 
+
+    // cout << pp[1]->get_name() << "\n";
+
+    // pp[0]-> print();            // calls Person::print()
+    // pp[1]->print();             // calls Person::print()
+
+    // // pp[1]->changeMajor("Math"); // ERROR !!!!
+    // /*
+    // - Since pp[1] is declared to be a pointer to a Person,
+    // the members for that class are used
+    // */
+
+
+   ////////////////////////// Template /////////////////////////////////////
+//    cout << genericMin(3, 4) << genericMin(1.1, 3.1) << genericMin('t', 'g') << endl;
+
+
+   ////////////////////////// Exceptions /////////////////////////////////////
+
+    double numerator = 12, denominator = 0;
+
+    try {
+        if (denominator == 0) throw ZeroDivision("Divide by zero !");
+    }
+    catch (ZeroDivision& zde) {
+        // handle division by zero
+    }
+    catch (MathException& me) {
+    // handle any math exception other than division by zero
+    }
 };
